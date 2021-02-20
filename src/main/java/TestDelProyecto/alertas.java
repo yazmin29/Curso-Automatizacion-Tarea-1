ac87c270b55b969d701c0a213228e9b581b6eda5
yazmin29 <yazmin.rodriguez.gutierrez@hotmail.com>
2021-02-19 19:18:53 -0600

    Nuevo

--- src/main/java/TestDelProyecto/alertas.java
+++ src/main/java/TestDelProyecto/alertas.java
@@ -1,11 +1,11 @@
 package TestDelProyecto;
 
 import org.testng.annotations.Test;
-import java.util.concurrent.TimeUnit;
+
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
